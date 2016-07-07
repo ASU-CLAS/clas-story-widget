5f978eaec097ff68d3841e11ea2006853a93bf7e
Sebastian Nievas <sebinievas@gmail.com>
2016-07-07 15:46:35 -0700

    Add selection handler

--- src/components/poll.jsx
+++ src/components/poll.jsx
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 
 export default class Poll extends Component {
+
   render() {
     return (
-      <div className="asu-widget-poll">
+      <div className="poll-question">
         <label>{this.props.question}</label>
-        <ul>{this.renderChoices()}</ul>
+        <select
+          className="form-control form-select"
+          onChange={this.handleSelection.bind(this)}
+          >
+          <option value="-1">Choose...</option>
+          {this.renderChoices()}
+        </select>
+        {this.renderDisclaimer()}
       </div>
     );
+  }
+
+  renderDisclaimer() {
+    return (this.props.note)
+      ? <p className="note">{this.props.note}</p>
+      : null;
   }
 
   renderChoices() {
     let i = 0;
     return this.props.choices.map(choice => {
-      return <li
+      return <option
         key={i++} 
         data-value={choice} 
-        onClick={this.handleSelection.bind(this)}
-      >{choice}</li>
+        value={choice}
+      >{choice}</option>
     });
   }
 
   handleSelection(event) {
-    this.props.handleSelection(event, event.target.innerHTML);
+    if (event.target.value)
+    this.props.handleSelection(event, event.target.value);
   }
 }
